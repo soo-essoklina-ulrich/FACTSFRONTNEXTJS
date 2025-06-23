500ead1ac52903d2251fa274f68384f823e45d0e
soo-essoklina-ulrich <sessoklinaulrich@gmail.com>
2025-06-23 01:55:46 +0000

    fix: update CircularProgress import and adjust size prop in LoadingWithoutModal component

--- src/components/LoadingWithoutModal.tsx
+++ src/components/LoadingWithoutModal.tsx
@@ -1,12 +1,12 @@
 'use client';
 
 import React from 'react';
-import { CircularProgress } from '@heroui/react';
+import { CircularProgress } from '@heroui/progress';
 
 const LoadingWithoutModal = ({ message = 'Chargement en cours...', padding = 'p-36' }) => {
   return (
     <div className={`flex flex-col items-center p-36 justify-items-center ${padding}`}>
-      <CircularProgress size={36} />
+      <CircularProgress size={'lg'} />
       <h6 className="mt-2">{message}</h6>
     </div>
   );
